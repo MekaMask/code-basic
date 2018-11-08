7a13fc30aaca1e01d4179fdd39903b6665fd53b0
maskmeka <agapevb35@gmail.com>
2018-11-08 17:48:04 +0700

    fail case

--- src/CodeBasic.Tests/UnitTest1.cs
+++ src/CodeBasic.Tests/UnitTest1.cs
@@ -10,9 +10,42 @@ namespace CodeBasic.Tests
         // Exception cases
 
         [Fact]
-        public void Test1()
+        public void P2Win()
         {
-
+            var Sut = new Pokdeng();
+            Sut.PlayerBalance = 200;
+            Sut.CheckGameResult(
+                            10,
+                            5, 1, 0, "", "", "",
+                            5, 4, 0, "", "", ""
+                            );
+            Assert.Equal(210, Sut.PlayerBalance);
         }
-    }
+
+        // [Fact]
+        // public void P1Win()
+        // {
+        //     var Sut = new Pokdeng();
+        // Sut.PlayerBalance = 200;
+        //     var result = Sut.CheckGameResult(
+        //                     10,
+        //                     5, 3, 0, "", "", "",
+        //                     5, 11, 0, "", "", ""
+        //                     );
+        // Assert.Equal(190, result);
+        // }
+
+        //         [Fact]
+        // public void P1AsP2()
+        // {
+        //     var Sut = new Pokdeng();
+        // Sut.PlayerBalance = 200;
+        //     var Result = Sut.CheckGameResult(
+        //                     10,
+        //                     5, 3, 0, "", "", "",
+        //                     5, 11, 1, "", "", ""
+        //                     );
+        // Assert.Equal(200, Result);
+        // }
+}
 }
--- src/CodeBasic/Pokdeng.cs
+++ src/CodeBasic/Pokdeng.cs
@@ -9,12 +9,45 @@ namespace CodeBasic
         public int PlayerBalance { get; set; }
 
         // Club, Diamond, Heart, Spade (case sensitive)
+        // 1 เป็นเจ้า
+        // 2 player
         public void CheckGameResult(
             int betAmount,
-            int p1CardNo1, int p1CardNo2, int p1CardNo3, 
+            int p1CardNo1, int p1CardNo2, int p1CardNo3,
             string p1CardSymbol1, string p1CardSymbol2, string p1CardSymbol3,
             int p2CardNo1, int p2CardNo2, int p2CardNo3,
             string p2CardSymbol1, string p2CardSymbol2, string p2CardSymbol3)
+        {
+            var p1 = plus(p1CardNo1, p1CardNo2, p1CardNo3);
+            var p2 = plus(p2CardNo1, p2CardNo2, p2CardNo3);
+
+            if (p1 < p2)
+            {
+                PlayerBalance += betAmount;
+            }
+            else if (p1 == p2)
+            {
+                PlayerBalance = PlayerBalance;
+            }
+            PlayerBalance -= betAmount;
+        }
+
+        public int plus(int CardNo1, int CardNo2, int CardNo3)
+        {
+            int Number = CardNo1 + CardNo2 + CardNo3;
+            if (Number > 9)
+            {
+                return Number - 9;
+            }
+            return Number;
+        }
+
+        public bool IscheckPok(int CardNo1, int CardNo2, int CardNo3)
+        {
+
+            throw new NotImplementedException();
+        }
+        public int pok8and9()
         {
             throw new NotImplementedException();
         }
